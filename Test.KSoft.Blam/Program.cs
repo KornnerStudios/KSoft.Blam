c7a67ece2bb47807fa5a352df32e2d888eb96fd6
kornman00 <kornman00@gmail.com>
2020-01-14 21:16:48 -0800

    Prepare Test.KSoft.Blam for Appveyor.

--- Test.KSoft.Blam/Program.cs
+++ Test.KSoft.Blam/Program.cs
@@ -13,14 +13,6 @@ namespace KSoft.Blam
 			KSoft.Program.Initialize();
 			KSoft.Blam.Program.Initialize();
 			Blam.Program.InitializeCoreSystems();
-
-			// If this isn't true, then whoever is using this code didn't update
-			// [kTestResultsPath] to reflect their project tree's test results dir.
-			//
-			// We don't use the TextContext's TestDir properties because there have
-			// been issues with VS using the "In" dir, and deleting whatever gets
-			// outputted into there. But hey, maybe I'm just doing something wrong!
-			Assert.IsTrue(System.IO.Directory.Exists(BaseTestClass.kTestResultsPath));
 		}
 		[AssemblyCleanup]
 		public static void AssemblyDispose()
@@ -36,8 +28,6 @@ namespace KSoft.Blam
 	[TestClass]
 	public abstract class BaseTestClass
 	{
-		public const string kTestResultsPath = @"C:\Mount\B\Kornner\Vita\_test_results\KSoft.Blam\";
-
 		/// <summary>
 		/// Gets or sets the test context which provides information about and functionality for the current test run.
 		/// </summary>
